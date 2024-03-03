8b41335a1203b84eae2d64349f78c8e17141e13a
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2024-03-03 12:23:10 -0300

    ng if e for

--- Angular/primeiro-projeto/src/app/componets/heroes/list/list.component.ts
+++ Angular/primeiro-projeto/src/app/componets/heroes/list/list.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  listaNomes:string[]=["clein","zem","fein","lein","sein"];
 }
