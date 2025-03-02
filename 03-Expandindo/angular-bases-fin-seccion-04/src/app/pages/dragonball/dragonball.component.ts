67c9cf4f3c3097d761deb01bc16882d94f2c33cd
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2025-03-02 18:46:38 -0300

    diretivas part 2

--- 03-Expandindo/angular-bases-fin-seccion-04/src/app/pages/dragonball/dragonball.component.ts
+++ 03-Expandindo/angular-bases-fin-seccion-04/src/app/pages/dragonball/dragonball.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Char{
+  id:number;
+  name:string;
+  power:number;
+}
 
 @Component({
   selector: 'app-dragonball',
   imports: [],
   templateUrl: './dragonball.component.html',
-  styleUrl: './dragonball.component.css'
+  styleUrl: './dragonball.component.css',
 })
 export class DragonballComponent {
-
+  name = signal('gohan');
+  power = signal(4500);
+  char = signal<Char[]>([
+    { id: 1, name: 'Goku', power: 9000 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 3, name: 'Yamcha', power: 500 },
+  ]);
 }
